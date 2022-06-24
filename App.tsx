f484ed7e4fe3f4b0d86b438bd8b9c3d79828bef3
Dương Quốc Hải <hai.duongquoc@hungthinhcorp.com.vn>
2022-06-24 22:39:18 +0700

    fix: scroll Carousel not working on android

--- App.tsx
+++ App.tsx
@@ -1,17 +1,18 @@
 import * as React from 'react';
 import {Text, View, StyleSheet, StatusBar} from 'react-native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import ProductDetailScreen from './src/screens/product-detail.screen';
 
 const App = () => {
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <StatusBar
         barStyle={'light-content'}
         backgroundColor="transparent"
         translucent
       />
       <ProductDetailScreen />
-    </View>
+    </GestureHandlerRootView>
   );
 };
 
